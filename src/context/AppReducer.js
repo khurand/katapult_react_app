3c8bd26af6aa0581818fff3b7a6619efb18d8493
H K <kevin.hurand@gmail.com>
2020-03-15 23:24:36 -0700

    hbkj

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,21 +1,24 @@
 // How to specify application state changes and response to certain actions to the context
-export default (state, action) => {
+export default (state = 0, action) => {
     switch(action.type) {
         case 'START_TIMER':
             return {
                 ...state,
-                chronos: // lancer le timer à partir de 60min et 0sec
+             // lancer le timer à partir de 60min et 0sec
+                // chronos: 
             }
-        case 'PAUSE_TIMER':
-            return {
-                ...state,
-                chronos: // pause le timer 
-            }
-        case 'STOP_TIMER':
-            return {
-                ...state,
-                chronos: // reset le timer à 60min et 0sec
-            } 
+        // case 'PAUSE_TIMER':
+        //     return {
+        //         ...state,
+        //      // pause le timer
+        //         chronos:  
+        //     }
+        // case 'STOP_TIMER':
+        //     return {
+        //         ...state,
+        //      // reset le timer à 60min et 0sec
+        //         chronos: 
+        //     } 
         default:
             return state;
     }
